38a30f19d2bd09f9ca8993e210314e6e0102c2b6
Sina Rismanchian <sina.rismanchian@gmail.com>
2019-06-12 01:23:40 +0430

    some queries for project data added.

---
+++ crowso/accounts/queries.py
@@ -0,0 +1,24 @@
+from django.db.models import Sum
+
+
+def get_contributed_percent(project):
+    sum_ = project.subproject_set.aggregate(percent=Sum('percent'))
+    return sum_['percent_sum']
+
+
+def get_done_percent(project):
+    sub_projects = project.subproject_set.all()
+    percent = 0
+    for sp in sub_projects:
+        if sp.revision.accepted:
+            percent += sp.percent
+    return percent
+
+
+def rejected_percent(project):
+    sub_projects = project.subproject_set.all()
+    percent = 0
+    for sp in sub_projects:
+        if sp.revision.is_rejected():
+            percent += sp.percent
+    return percent

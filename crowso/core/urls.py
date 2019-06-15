aa84a69aba81b3963e12550150dcb31070cf329a
farhadierf <farhadi.erfan@yahoo.com>
2019-06-16 02:43:42 +0430

    added home. everything needs خوشگلاسیون

--- crowso/core/urls.py
+++ crowso/core/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='crowso/base.html'), name='home'),
+    path('', views.home, name='home'),
     path('create_project', views.project_creation_view, name='create_project'),
 ]
--- crowso/core/views.py
+++ crowso/core/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 
 from . import forms
 
-
 def project_creation_view(request):
     if request.POST:
         form = forms.ProjectCreationForm(request.POST, request.FILES)
@@ -16,3 +15,14 @@ def project_creation_view(request):
     else:
         form = forms.ProjectCreationForm()
         return render(request, 'core/create_project.html', {'form': form})
+
+
+def home(request):
+
+    def get_user_projects(user):
+        import datetime
+        lipsum = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        return [{'name': 'ali', 'value': 10, 'description': lipsum, 'deadline': datetime.date.today}, {'name': 'bali', 'value': 30, 'description': lipsum, 'deadline': datetime.date.today}, {'name': 'vali', 'value': 40, 'description': lipsum, 'deadline': datetime.date.today}]
+    user = request.user
+    projects = get_user_projects(user)
+    return render(request, 'core/home.html', {'projects': projects})

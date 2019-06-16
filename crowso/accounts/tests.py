9f8f9888aa2a5e9d8479f501731969b4303dbbb1
farhadierf <farhadi.erfan@yahoo.com>
2019-06-16 16:02:33 +0430

    added unit test. everything needs خوشگلاسیون

--- crowso/accounts/tests.py
+++ crowso/accounts/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+import datetime
+import random
+from accounts.forms import UserSignupForm
+
 
 # Create your tests here.
+
+class TestFormProject(TestCase):
+    def test_signup_form_valid(self):
+        upload_file = open('static/assets/image/banner.jpg', 'rb')
+        img = SimpleUploadedFile(upload_file.name, upload_file.read())
+
+        form = UserSignupForm(files={'picture': img}, data={
+            'name': 'a', 'username': 'ab', 'email': 'ab@u.ic', 'password1': 'aabbaabb', 'password2': 'aabbaabb',
+            'phone_number': '09091234567', 'address': 'b'})
+        self.assertTrue(form.is_valid())
+
+        pn = '09' + ''.join(str(random.randint(1, 9)) for i in range(9))
+        form = UserSignupForm(files={'picture': img}, data={
+            'name': 'a', 'username': 'ab', 'email': 'ab@u.ic', 'password1': 'aabbaabb', 'password2': 'aabbaabb',
+            'phone_number': pn, 'address': 'b'})
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+    def test_signup_form_invalid(self):
+        form = UserSignupForm(data={}, files={})
+        print(form.errors)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 7)
+
+        upload_file = open('static/assets/image/banner.jpg', 'rb')
+        img = SimpleUploadedFile(upload_file.name, upload_file.read())
+        form = UserSignupForm(files={'picture': img}, data={'password1': 'aabbaabb', 'password2': 'aabbaabb',
+            'name': 'a', 'username': 'ab', 'email': 'ab', 'phone_number': '09091234567', 'address': 'b'})
+        print(form.errors)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+        form = UserSignupForm(files={'picture': img}, data={'password1': 'abbbaa2', 'password2': 'abbbaa2',
+            'name': 'a', 'username': 'ab', 'email': 'ab@c.ir', 'phone_number': '09091234567', 'address': 'b'})
+        print(form.errors)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
--- crowso/core/tests.py
+++ crowso/core/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import random
+from django.test import SimpleTestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+import datetime
+from core.forms import ProjectCreationForm
 
-# Create your tests here.
+
+class TestFormProject(SimpleTestCase):
+    def test_creation_project_form_valid(self):
+        upload_file = open('static/assets/image/logo.png', 'rb')
+        img = SimpleUploadedFile(upload_file.name, upload_file.read())
+        form = ProjectCreationForm(files={'attachment': img, 'picture': img}, data={
+            'name': 'a', 'description': 'b', 'value': 20, 'type': 3, 'deadline': datetime.date.today()})
+        self.assertTrue(form.is_valid())
+        form = ProjectCreationForm(files={'attachment': img, 'picture': img}, data={
+            'name': 'a', 'description': 'b', 'value': random.randint(1, 1000), 'type': random.randint(1, 3),
+            'deadline': datetime.date.today() - datetime.timedelta(days=random.randint(1, 50))})
+        self.assertTrue(form.is_valid())
+
+    def test_creation_project_from_form_invalid(self):
+        form = ProjectCreationForm(data={}, files={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 7)
